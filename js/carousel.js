9d48dc5bd6ce038bebea40bf875c0111cfacb9b6
ctlemos <catarinatlemost@gmail.com>
2025-03-02 20:41:27 +0000

    table removed and added a grid with the last youtube videos - in the about page

--- js/carousel.js
+++ js/carousel.js
@@ -1,3 +1,27 @@
+/* VIDEO GRID */
+const videos = [
+    { id: 1, title: "Vila de Rei (walkways and landscapes)", thumbnail: "https://img.youtube.com/vi/Hr0PFvCieT8/maxresdefault.jpg", url: "https://www.youtube.com/watch?v=Hr0PFvCieT8" },
+    { id: 2, title: "Nature takes over", thumbnail: "https://img.youtube.com/vi/mHqGW180Ils/maxresdefault.jpg", url: "https://www.youtube.com/watch?v=mHqGW180Ils" },
+    { id: 3, title: "Ribeira de Fráguas", thumbnail: "https://img.youtube.com/vi/6AJAvJnu7zw/maxresdefault.jpg", url: "https://www.youtube.com/watch?v=6AJAvJnu7zw" }
+];
+
+const gridContainer = document.getElementById('grid-container');
+const carouselContainer = document.getElementById('carousel-container');
+const timelineContainer = document.getElementById('timeline-container');
+
+videos.forEach((video, index) => {
+    // DINAMIC GRID
+    gridContainer.innerHTML += `
+        <div class="col-md-4">
+            <a href="${video.url}" target="_blank" class="video-card d-block">
+                <img src="${video.thumbnail}" class="w-100">
+                <div class="video-overlay">${video.title}</div>
+            </a>
+        </div>`;
+});
+
+
+/* IMG CAROUSEL */
 document.addEventListener("DOMContentLoaded", function () {
     let modalImage = document.getElementById("modalImage");
     let galleryImages = document.querySelectorAll(".gallery-img");
